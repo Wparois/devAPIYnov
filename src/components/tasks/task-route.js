ba239c7f0f295cef036b317f288ede220276b25c
MansourWolou <Wolou0444@gmail.com>
2022-11-21 09:47:38 +0100

    update

--- src/components/tasks/task-route.js
+++ src/components/tasks/task-route.js
@@ -4,13 +4,9 @@ import * as TaskControllers from '#components/tasks/tasks-controllers.js'
 const tasks = new Router()
 
 tasks.get('/', TaskControllers.findAll)
-
 tasks.get('/:id', TaskControllers.findById)
-
 tasks.post('/', TaskControllers.create)
-
 tasks.put('/:id', TaskControllers.update)
-
 tasks.delete('/:id', TaskControllers.deleteById)
 
 export default tasks
